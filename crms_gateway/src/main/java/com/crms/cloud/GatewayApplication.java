b8e37cc1e901d21bf78ed72deef3987fe918912b
jcc <646524128@qq.com>
2025-04-09 15:27:12 +0800

    更新

---
+++ crms_gateway/src/main/java/com/crms/cloud/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.crms.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    void test(){
+        ThreadLocal<String> threadLocal=new ThreadLocal<>();
+    }
+
+}
